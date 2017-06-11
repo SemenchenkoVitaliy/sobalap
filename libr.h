dc66851f59b4c3aa9e4c434df171dda071535482
SemenchenkoVitaliy <semenchenkovitaliy@gmail.com>
2017-06-11 23:41:13 +0300

    Fix class interface and gif saving

--- libr.h
+++ libr.h
@@ -1,4 +1,47 @@
 #ifndef LIBR_H
 #define LIBR_H
-int testSob(int argc, char* argv[]);
+#define PLB_USE_POSIX
+#include "palabos2D.h"
+#include "palabos2D.hh"
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace plb;
+using namespace plb::descriptors;
+using namespace std;
+
+typedef double T;
+#define DESCRIPTOR D2Q9Descriptor
+
+enum InletOutletT {pressure, velocity};
+
+class Setup{
+public:
+    Setup();
+    void setInlet(int x1, int y1, int x2, int y2);
+    void setOutlet(int x1, int y1, int x2, int y2);
+    void setCalcPoint(int x, int y);
+    void setInletOutlet(std::string s);
+    void setTimeInterval(double t, double i);
+    void setSizes(double lx, double ly);
+    void setOutDir(std::string str);
+    void setOutFileName(std::string str);
+    void exec();
+private:
+    Box2D inlet, outlet;
+    plint Nx, Ny;
+    plint xCalc, yCalc;
+    InletOutletT inletOutlet;
+    T imSave, maxT, lx, ly;
+    std::string outDir, outFileName;
+
+    SparseBlockStructure2D sparseSetup();
+    void channelSetup( MultiBlockLattice2D<T,DESCRIPTOR>& lattice,
+                   IncomprFlowParam<T> const& parameters,
+                   OnLatticeBoundaryCondition2D<T,DESCRIPTOR>& boundaryCondition);
+    void writeGif(MultiBlockLattice2D<T,DESCRIPTOR>& lattice, plint iter);
+};
 #endif // LIBR_H
--- main.cpp
+++ main.cpp
@@ -3,7 +3,17 @@
 #include "libr.h"
 
 int main(int argc, char *argv[]) {
-  testSob(argc, argv);
+  Setup* obj = new Setup();
+  obj->setInlet(0, 0, 20, 600);
+  obj->setOutlet(780, 0, 800, 600);
+  obj->setCalcPoint(40, 30);
+  obj->setInletOutlet("pressure");
+  obj->setTimeInterval(1.0, 0.05);
+  obj->setSizes(8., 6.);
+  obj->setOutDir(".\\tmp");
+  obj->setOutFileName("result.gif");
+  obj->exec();
+
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
